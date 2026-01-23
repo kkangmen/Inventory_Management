108ff8f632c8b2267a6cd6fa9a2a0fa97904f038
kkangmen <pickofee@gmail.com>
2026-01-23 23:41:07 +0900

    [refactor] 코드 정리

--- src/main/java/myProject/toyproject/ToyprojectApplication.java
+++ src/main/java/myProject/toyproject/ToyprojectApplication.java
@@ -3,6 +3,7 @@ package myProject.toyproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ToyprojectApplication {
@@ -10,5 +11,6 @@ public class ToyprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToyprojectApplication.class, args);
 	}
+
 
 }
