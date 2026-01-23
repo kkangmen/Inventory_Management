d670f9c327d64d0a2f30daf21455dce31a605ab7
kkangmen <pickofee@gmail.com>
2026-01-23 23:40:07 +0900

    [test] PostConstruct 테스트

---
+++ src/main/java/myProject/toyproject/TestDataInit.java
@@ -0,0 +1,36 @@
+package myProject.toyproject;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import myProject.toyproject.item.dto.ItemCreateRequest;
+import myProject.toyproject.item.repository.MemoryItemRepository;
+import myProject.toyproject.item.service.ItemServiceImpl;
+import myProject.toyproject.member.dto.MemberCreateRequest;
+import myProject.toyproject.member.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemServiceImpl itemService;
+    private final MemoryMemberRepository memberRepository;
+
+    @PostConstruct
+    public void init(){
+
+        /***
+         * 더미 데이터
+         */
+        ItemCreateRequest itemD = new ItemCreateRequest("itemD", 40000, 400);
+        ItemCreateRequest itemE = new ItemCreateRequest("itemE", 50000, 500);
+        ItemCreateRequest itemF = new ItemCreateRequest("itemF", 60000, 600);
+
+        itemService.createItem(itemD);
+        itemService.createItem(itemE);
+        itemService.createItem(itemF);
+
+        MemberCreateRequest memberCreateRequest = new MemberCreateRequest("test", "test", "tester");
+        memberRepository.save(memberCreateRequest);
+    }
+}
