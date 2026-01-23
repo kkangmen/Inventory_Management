2a592bd4e90631ddcb38538c2eeeace6a2354178
kkangmen <pickofee@gmail.com>
2026-01-23 23:43:49 +0900

    [refactor] PostConstruct 이동

--- src/main/java/myProject/toyproject/item/controller/ItemController.java
+++ src/main/java/myProject/toyproject/item/controller/ItemController.java
@@ -130,18 +130,4 @@ public class ItemController {
         itemService.updateItem(itemId, form);
         return "redirect:/api/items/{itemId}";
     }
-
-    /***
-     * 더미 데이터
-     */
-    @PostConstruct
-    public void init(){
-        ItemCreateRequest itemD = new ItemCreateRequest("itemD", 40000, 400);
-        ItemCreateRequest itemE = new ItemCreateRequest("itemE", 50000, 500);
-        ItemCreateRequest itemF = new ItemCreateRequest("itemF", 60000, 600);
-
-        itemService.createItem(itemD);
-        itemService.createItem(itemE);
-        itemService.createItem(itemF);
-    }
 }
