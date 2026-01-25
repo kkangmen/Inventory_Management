542a119114bea7001dc8161cf0262bb978ff3911
kkangmen <pickofee@gmail.com>
2026-01-25 23:32:17 +0900

    [fix] Bean Validation 어노테이션 수정

--- src/main/java/myProject/toyproject/login/dto/LoginForm.java
+++ src/main/java/myProject/toyproject/login/dto/LoginForm.java
@@ -1,14 +1,15 @@
 package myProject.toyproject.login.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class LoginForm {
 
-    @NotNull(message = "로그인 아이디를 입력해주세요.")
+    @NotBlank(message = "로그인 아이디를 입력해주세요.")
     private String loginId;
 
-    @NotNull(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
